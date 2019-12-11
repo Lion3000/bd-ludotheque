85bca946653d767acaed76cfabb8f92fbe34ce5f
MaelVB <vaillant.beuchot.mael@gmail.com>
2019-12-11 17:20:11 +0100

    Correction de l'utilisation de la nested table et de l'héritage

--- article_et_film.sql
+++ article_et_film.sql
@@ -1,4 +1,6 @@
-// Création du type article
+drop table ARTICLE_O;
+
+/* Création du type article */
 create or replace type ARTICLE_T AS OBJECT (
     ARTICLEID	number(8),
     TITRE   varchar2(15),
@@ -8,38 +10,36 @@ create or replace type ARTICLE_T AS OBJECT (
     DATEPARUTION date,
     
     ORDER MEMBER function compArticle (article IN ARTICLE_T) return number
-);
+)NOT FINAL;
 
-// Création de la liste de référence des articles
+/* Création de la liste de référence des articles */
 create or replace type listRefArticle_t as table of REF ARTICLE_T;
 
-// Création de la table objet article avec ses contraintes
+/* Création de la table objet article avec ses contraintes */
 create table article_o of article_t(
 	CONSTRAINT pk_article_o_articleid PRIMARY KEY(ARTICLEID),
 	CONSTRAINT nnl_article_o_titre TITRE NOT NULL,
 	CONSTRAINT nnl_article_o_genre GENRE NOT NULL,
 	CONSTRAINT nnl_article_o_ageminimal AGEMINIMAL NOT NULL,
 	CONSTRAINT nnl_article_o_editeur EDITEUR NOT NULL,
-	CONSTRAINT nnl_article_o_dateparution DATEPARUTION NOT NULL,
-	CONSTRAINT chk_article_o_dateparution CHECK(DATEPARUTION <= NOW())
+	CONSTRAINT nnl_article_o_dateparution DATEPARUTION NOT NULL
+	/*CONSTRAINT chk_article_o_dateparution CHECK(DATEPARUTION <= NOW())*/
 );
 
-// ----------------------
-create or replace type nested_table_film_type IS TABLE OF varchar2(15);
+/* -------------------- */
+create or replace type ActeursPrinc_t IS TABLE OF varchar2(15);
 
-// Création du type film
+/* Création du type film */
 create or replace type FILM_T UNDER ARTICLE_T (
     REALISATEUR   varchar2(15),
-    ACTEURS_PRINC   nested_table_film_type,
-    
-    ORDER MEMBER function compFilm (film IN FILM_T) return number
+    ACTEURS_PRINC   ActeursPrinc_t
 );
 
-// Création de la liste de référence des films
+/* Création de la liste de référence des films */
 create or replace type listRefFilm_t as table of REF FILM_T;
 
-// Création de la table objet film avec ses contraintes
+/* Création de la table objet film avec ses contraintes */
 create table film_o of film_t(
-	CONSTRAINT nnl_film_o_realisateur REALISATEUR NOT NULL,
-	CONSTRAINT nnl_film_o_acteurs_princ ACTEURS_PRINC NOT NULL
-);
+	CONSTRAINT nnl_film_o_realisateur REALISATEUR NOT NULL
+)
+nested table ACTEURS_PRINC store as STOREACTEURS_PRINC;
